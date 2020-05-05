51f54fcc8ac7150e55f7b1796de40c390b3f79a9
Jon Cornwell <jonnmb@gmail.com>
2020-05-04 18:07:04 -0600

    Move Table Row into its own component

---
+++ src/components/TableRow/TableRow.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const TableRows = (props: any) => {
+	const { columnKeys, rowData } = props;
+
+	useEffect(() => {
+		console.log('render');
+	});
+
+	return rowData?.map((item: any) => {
+		return (
+			<tr className={'table-element-row'} key={item.id}>
+				{columnKeys.map((key: string) => {
+					const elementKey = `${item.id}-${item[key]}`;
+					const classNames = `table-element-cell ${key}`;
+					const value = item[key].toString();
+					if (key === 'name') {
+						return (
+							<td key={elementKey} className={classNames}>
+								<Link to={{ pathname: '/details', state: { item } }}>{value}</Link>
+							</td>
+						);
+					}
+					return (
+						<td key={elementKey} className={classNames}>
+							{value}
+						</td>
+					);
+				})}
+			</tr>
+		);
+	});
+};
+
+export default TableRows;
